c1aa8cf64b06bc5de61194b2e46496377293d837
Khatai Suleymanli <145442643+HajiyevH@users.noreply.github.com>
2025-04-13 02:05:25 +0200

    game start button

---
+++ Assets/Scripts/mainmenu.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class mainmenu : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadSceneAsync(0);
+    }
+}
